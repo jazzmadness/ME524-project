74da00f463dc6c3079754ed7c65c6427c7080926
leonardotorin <leonardo_torin@yahoo.com.br>
2016-06-01 12:48:13 -0300

    Script do R com obtenção de dados aleatórios do banco de dados e protótipo de estatística descritiva de frequência.

--- Projeto.R
+++ Projeto.R
@@ -5,3 +5,5 @@ data = dbConnect(SQLite(), dbname = "train.sqlite3")
 dbGetInfo(data)
 head_data = dbGetQuery(data, "SELECT * FROM train LIMIT 100")
 head_data[-1,] #solução temporária para os headers duplos.
+data_random = dbGetQuery(data, "SELECT * FROM train WHERE random() % 100000 = 0 LIMIT 1000000")
+View(table(data_random[[2]]))
