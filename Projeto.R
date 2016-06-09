1043008195bb0f9c641261c42e6378ce971dff52
leonardotorin <leonardo_torin@yahoo.com.br>
2016-06-09 20:40:54 -0300

    Correção no nome das variáveis.

--- Projeto.R
+++ Projeto.R
@@ -1,16 +1,16 @@
 dir = getwd()
 setwd(dir)
 library(RSQLite)
-databank = dbConnect(SQLite(), dbname = "train.sqlite3")
-dbGetInfo(databank)
-head_data = dbGetQuery(databank, "SELECT * FROM train LIMIT 100")
+database = dbConnect(SQLite(), dbname = "train.sqlite3")
+dbGetInfo(database)
+head_data = dbGetQuery(database, "SELECT * FROM train LIMIT 100")
 head_data = head_data[-1,] #solução para os headers duplos.
 ## 1a solucao para amostragem aleatoria
-data_random = dbGetQuery(databank, "SELECT * FROM train WHERE random() % 100 = 0 LIMIT 1000")
+data_random = dbGetQuery(database, "SELECT * FROM train WHERE random() % 100 = 0 LIMIT 1000")
 View(table(data_random[[2]]))
 #2a solucao para amostragem aleatoria
 n = 37670294 #tamanho de train
 indices_random = data.frame(sample(n, 1e6))
 query_random = "SELECT * from train WHERE ROWID = ?"
-data_random_2 = dbGetPreparedQuery(databank, query_random, indices_random)
+data_random_2 = dbGetPreparedQuery(database, query_random, indices_random)
 View(data_random_2)
